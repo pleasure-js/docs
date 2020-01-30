d151676e72ff41d59c33ccf654c37f476a7a3326
Martin Rafael Gonzalez <tin@devtin.io>
2020-01-30 08:16:42 -0500

    fix: matching new 'rollup-plugin-alias' syntax

--- rollup.config.js
+++ rollup.config.js
@@ -12,8 +12,20 @@ const fromSrc = (...paths) => {
 
 const plugins = [
   alias({
-    src: fromSrc(),
-    lib: fromSrc('lib')
+    entries: [
+      {
+        find: 'src',
+        replacement: fromSrc()
+      },
+      {
+        find: 'lib',
+        replacement: fromSrc('lib')
+      },
+      {
+        find: 'utils',
+        replacement: fromSrc('utils')
+      }
+    ]
   }),
   json(),
   commonjs({
