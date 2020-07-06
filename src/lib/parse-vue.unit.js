f099b26b057d4c86618a3f595f956f1035567e4d
Martin Rafael Gonzalez <tin@devtin.io>
2020-07-06 09:26:09 -0400

    test: snapshot update

--- src/lib/parse-vue.unit.js
+++ src/lib/parse-vue.unit.js
@@ -1,9 +1,13 @@
 import { parseVue } from './parse-vue'
+import omit from 'lodash/omit'
 import test from 'ava'
 import fs from 'fs'
 
 test('Converts vue components into json', t => {
   const converted = parseVue(fs.readFileSync(benchmark('vue-component.vue')).toString())
   t.truthy(converted)
+  converted.script.jsdoc = converted.script.jsdoc.map(obj => {
+    return omit(obj, 'meta.filename')
+  })
   t.snapshot(converted)
 })
